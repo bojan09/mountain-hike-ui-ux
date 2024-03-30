0a6b14913c2eb9d981aa7efb6a32f9435cc6be8b
Bojan Stanimirovski <bojan-stanimirovski097@hotmail.com>
2024-03-30 13:38:25 +0100

    setting up folders and files, adding image assets

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 export default function Home() {
-  return <h1 className="text-teal-500">Hello world</h1>;
+  return <h1 className="text-bold text-teal-500 text-2xl">Hello world</h1>;
 }
