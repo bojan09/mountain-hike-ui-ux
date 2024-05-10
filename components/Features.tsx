85ba64969b48435e9ad2345683805a6be2c94f5b
bojan09 <bojan-stanimirovski097@hotmail.com>
2024-05-10 09:16:10 +0200

    starting Features section

--- components/Features.tsx
+++ components/Features.tsx
@@ -1,7 +1,19 @@
-import React from "react";
+import Image from "next/image";
 
 const Features = () => {
-  return <div>Features</div>;
+  return (
+    <section className="border-2 border-red-200 flex-col flexContainer overflow-hidden bg-feature-bg bg-center bg-no-repeat py-24">
+      <div className="max-container padding-container relative w-full justify-end">
+        <Image
+          src="/phone.png"
+          alt="phone"
+          width={440}
+          height={1000}
+          className="feature-phone"
+        />
+      </div>
+    </section>
+  );
 };
 
 export default Features;
